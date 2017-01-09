b74e366c36bb60b725597b6f5042fadb2e54dff4
shihy52x <shihongbo1234@gmail.com>
2017-01-08 22:07:20 -0500

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,8 @@
+gcc -c GA.c
+ 
 gcc -c  read.c
-g++   -I/home/ashwin/lammps-6Apr13/src/STUBS -I/home/ashwin/lammps-6Apr13/src #
-gcc read.o  atom.o cal.o -lstdc++   -I/home/ashwin/lammps-6Apr13/src/STUBS -I/#
-rm *.o
+ 
+g++   -I/home/ashwin/lammps-6Apr13/src/STUBS -I/home/ashwin/lammps-6Apr13/src -L/home/ashwin/lammps-6Apr13/src /home/ashwin/lammps-6Apr13/src/STUBS/libmpi_stubs.a  -llammps_serial -lfftw3 -lm -lstdc++   -lpthread -c lammps.cpp #
+
+gcc read.o  Lammps.o GA.o -lstdc++   -I/home/ashwin/lammps-6Apr13/src/STUBS -I/home/ashwin/lammps-6Apr13/src -L/home/ashwin/lammps-6Apr13/src /home/ashwin/lammps-6Apr13/src/STUBS/libmpi_stubs.a  -llammps_serial -lfftw3 -lm -lstdc++  -lrt  -lpthread-#
+
