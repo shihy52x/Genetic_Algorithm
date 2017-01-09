b55813293043f60f8c5b9e9775468bc3a044fd67
shihy52x <shihongbo1234@gmail.com>
2017-01-08 21:57:18 -0500

    Create compile.sh
    bash file for compiling the code. Please change the directories of the libraries  .

---
+++ compile.sh
@@ -0,0 +1,4 @@
+gcc -c  read.c
+g++   -I/home/ashwin/lammps-6Apr13/src/STUBS -I/home/ashwin/lammps-6Apr13/src #
+gcc read.o  atom.o cal.o -lstdc++   -I/home/ashwin/lammps-6Apr13/src/STUBS -I/#
+rm *.o
